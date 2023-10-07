25cdd292b43413f9b18eec37da1233513c84b3a3
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2023-10-07 15:32:31 -0400

    Added colors back

--- Sims1WidescreenPatcher.UI/Converters/ResolutionColorCodingConverter.cs
+++ Sims1WidescreenPatcher.UI/Converters/ResolutionColorCodingConverter.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Globalization;
+using Avalonia;
 using Avalonia.Data.Converters;
+using Avalonia.Media;
+using Avalonia.Styling;
 using Sims1WidescreenPatcher.Core.Models;
-using Sims1WidescreenPatcher.UI.Utilities;
 
 namespace Sims1WidescreenPatcher.UI.Converters;
 
@@ -10,7 +12,38 @@ public class ResolutionColorCodingConverter : IValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        return value is not Resolution r ? null : ResolutionColorThemeUtility.GetColor(r.AspectRatio);
+        if (value is not Resolution r)
+        {
+            return null;
+        }
+
+        if (r.Width % 1600 == 0)
+        {
+            if (Application.Current?.ActualThemeVariant == ThemeVariant.Light)
+            {
+                return Brush.Parse("#f66");
+            }
+
+            if (Application.Current?.ActualThemeVariant == ThemeVariant.Dark)
+            {
+                return Brush.Parse("#ff1e1e");
+            }
+        }
+        
+        if (r.AspectRatio is { Numerator: 4, Denominator: 3 })
+        {
+            if (Application.Current?.ActualThemeVariant == ThemeVariant.Light)
+            {
+                return Brush.Parse("#97ef7f");
+            }
+
+            if (Application.Current?.ActualThemeVariant == ThemeVariant.Dark)
+            {
+                return Brush.Parse("#165607");
+            }
+        }
+
+        return null;
     }
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
