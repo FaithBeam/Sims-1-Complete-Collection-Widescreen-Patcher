b1b4dc1d29b993b9f903ab760befdd73c4cd4932
FaithBeam <32502411+FaithBeam@users.noreply.github.com>
2023-10-07 10:53:55 -0400

    Prevent aspect ratio combo from growing/shrinking Throttle SelectedResolution update from updated FilteredResolutions to once every 100 milliseconds for performance reasons

--- Sims1WidescreenPatcher.Core/ViewModels/MainWindowViewModel.cs
+++ Sims1WidescreenPatcher.Core/ViewModels/MainWindowViewModel.cs
@@ -98,6 +98,7 @@ public class MainWindowViewModel : ViewModelBase, IMainWindowViewModel
             .Filter(resolutionFilter)
             .Sort(resolutionSort)
             .Bind(out _filteredResolutions)
+            .Throttle(TimeSpan.FromMilliseconds(100))
             .Subscribe(x => SelectedResolution = FilteredResolutions.First());
         _resolutionSource
             .Connect()
